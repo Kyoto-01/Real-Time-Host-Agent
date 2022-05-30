d100e06949c4225374f5ee4870afd4332b86cfab
Jônatas Mendes da Cruz <62781857+Kyoto-01@users.noreply.github.com>
2022-05-29 22:59:04 -0300

    Update sysinfo_collector.js

--- src/sysinfo_collector.js
+++ src/sysinfo_collector.js
@@ -1,60 +1,84 @@
-import si from 'systeminformation';
+import sysinfo from 'systeminformation';
 
 
-async function get_os_infos(){
-    const data = await si.osInfo()
-    return data
+async function get_sysinfo() {
+  const os = await sysinfo.osInfo();
+  const memory = await sysinfo.mem();
+  const cpu = await sysinfo.cpu();
+  const cpuLoad = await sysinfo.currentLoad();
+  const cpuTemperature = await sysinfo.cpuTemperature();
+  const nicsGeneral = await sysinfo.networkInterfaces();
+  const nicsStats = await sysinfo.networkStats();
+  const filesystems = await sysinfo.fsSize();
+
+  const info_obj =
+  {
+    "general": {
+      "hostname": os.hostname,
+      "ip": nicsGeneral[1].ip4,
+      "os": os.distro,
+      "online": true
+    },
+    "memory": {
+      "total": memory.total,
+      "used": memory.used,
+      "available": memory.free
+    },
+    "cpu": {
+      "model": cpu.brand,
+      //"architecture": cpu.manufacturer,
+      "cores": cpu.physicalCores,
+      "threads_core": (cpu.cores / cpu.physicalCores),
+      "sockets": cpu.processors,
+      "used": cpuLoad.currentLoad,
+      "temperature": cpuTemperature.main,
+      "clock": cpu.speed,
+      "clock_max": cpu.speedMax,
+      "clock_min": cpu.speedMin
+    },
+    "nic": get_nics(nicsGeneral, nicsStats),
+    "devices": get_filesystems(filesystems),
+  }
+
+  return info_obj
 }
 
-async function get_cpu_infos(){
-    const data = await si.cpu()
-    return data
+function get_nics(nicsGeneral, nicsStats) {
+  const nicList = [];
+
+  for (let nic in nicsGeneral) {
+    const nicInfo = {
+      "name": nicsGeneral[nic].iface,
+      "ip": nicsGeneral[nic].ip4,
+      "mask": nicsGeneral[nic].ip4subnet,
+      "mac": nicsGeneral[nic].mac,
+      "tx_pckts": nicsStats[nic - 1]?.tx_bytes || 0,
+      "rx_pckts": nicsStats[nic - 1]?.rx_bytes || 0
+    };
+
+    nicList.push(nicInfo);
+  }
+
+  return nicList;
 }
 
-async function get_cpu_infos2(){
-    const data = await si.currentLoad()
-    return data
-}
+function get_filesystems(filesystems) {
+  const fsList = [];
 
-async function get_cpu_infos3(){
-    const data = await si.cpuTemperature()
-    return data
-}
+  for (let fs in filesystems) {
+    const fsInfo = {
+      "name": filesystems[fs].fs,
+      "used": filesystems[fs].used,
+      "available": filesystems[fs].available,
+      "total": filesystems[fs].size,
+      "mount_point": filesystems[fs].mount
+    };
 
-async function get_cpu_infos4(){
-    const data = await si.cpuCurrentSpeed()
-    return data
-}
+    fsList.push(fsInfo);
+  }
 
-async function get_memory_infos(){
-    const data = await si.mem()
-    return data
-}
-
-async function get_nic_infos(){
-    const data = await si.networkInterfaces()
-    return data
-}
-
-async function get_nic_infos2(){
-    const data = await si.networkStats()
-    return data
-}
-
-async function get_vol_infos(){
-    const data = await si.fsSize()
-    return data
+  return fsList;
 }
 
 
-export default { 
-    get_os_infos,
-    get_cpu_infos,
-    get_cpu_infos2,
-    get_cpu_infos3,
-    get_cpu_infos4,
-    get_nic_infos,
-    get_nic_infos2,
-    get_memory_infos,
-    get_vol_infos
-};
+export default { get_sysinfo };
